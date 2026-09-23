898b77d8d5c3b8d74ab69294afb144d7507c6d20
punnnana <67051158@kmitl.ac.th>
2026-09-24 00:45:38 +0700

    Add cell06 ex02 downcase_all.py

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env py
+import sys
+
+def downcase_it(s):
+    return s.lower()
+
+params = sys.argv[1:]
+if not params:
+    print("none")
+else:
+    for p in params:
+        print(downcase_it(p))
