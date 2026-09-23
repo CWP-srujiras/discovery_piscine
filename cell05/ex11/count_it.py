7ddd671abc1e06afe9671188473945810f5cf461
punnnana <67051158@kmitl.ac.th>
2026-09-24 00:23:31 +0700

    Add cell05 ex11 count_it.py

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env py
+import sys
+params = sys.argv[1:]
+if not params:
+    print("none")
+else:
+    print(f"parameters: {len(params)}")
+    for p in params:
+        print(f"{p}: {len(p)}")
